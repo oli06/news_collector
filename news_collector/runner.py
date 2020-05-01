addf31badcfd89c7b460b8b80114b095a950c7e7
oli_zernikow <zernikowoliver@gmail.com>
2020-05-01 22:26:27 +0200

    scrapy init, n-tv crawler

---
+++ news_collector/runner.py
@@ -0,0 +1,25 @@
+#used to debug from vs code.
+#otherwise just use 'scrapy crawl news' from commandline
+
+import os
+from scrapy.cmdline import execute
+import subprocess
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+try:
+    output_filename = "out.json"
+    subprocess.check_call(['rm', output_filename])
+    #execute(["rm", output_filename] )
+    execute(
+        [
+            'scrapy',
+            'crawl',
+            'news',
+            "-t", "json",
+            '-o',
+            output_filename
+        ]
+    )
+except SystemExit:
+    pass
---
+++ news_collector/spiders/news_spider.py
@@ -0,0 +1,90 @@
+import scrapy
+
+
+class NewsSpider(scrapy.Spider):
+    name = "news"
+    def start_requests(self):
+        urls = [
+            "https://www.n-tv.de/"
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+
+    def parse(self, response):
+        #n-tv.de
+        content = response.xpath(
+            '//body/div[@class="metawrapper"]/div[@class="container sitewrapper"]/div[@class="row "]/div')
+
+        top_news = content.xpath(
+            '//div[@class="content "]/section[@class="group"]/article')  # NACHRICHTEN
+
+        for article in top_news:
+            content = article.xpath('.//div[@class="teaser__content"]')
+            href = content.css('div.teaser__content a::attr(href)').get()
+            yield response.follow(href, callback=self.parseArticle)
+
+    def parseArticle(self, response):
+        #print("parse article")
+        article = response.xpath('//article[@class="article"]')
+        
+        if len(article) == 0: #there a webpages that are not news articles
+            return 
+
+        article_wrapper = article.css('div.article__wrapper')
+        header = article_wrapper.css('div.article__header')
+
+        text = article_wrapper.xpath('.//div[@class="article__text"]/p')
+        named_references = {}
+        article_text_blocks = []
+        block_index = 0
+        for t in text[1:]: #the first paragraph is normally the teaser_text
+            nodes = t.xpath('.//node()')
+            article_text_blocks.append([])
+            for node in nodes:
+                if node.xpath('name()').get() == 'a':
+                    # save reference link in named_references and dont save the link to the text blocks
+                    href = node.xpath('@href').get()
+                    if not href.endswith(".html"):
+                        continue
+                    named_references[node.xpath('text()').get()] = href
+                    yield response.follow(node, callback=self.parseArticle)
+                else:
+                    article_text_blocks[block_index].append(node.get().strip().lower())
+
+            block_index += 1
+
+        authors = header.css(
+                'span.article__author::text').getall()
+        if len(authors) == 0:
+                #maybe it´s a newer styling, where the author is linked with a <a> tag
+                #maybe there´s no author
+                author_name = header.css('span.article__author a::text').get()
+                if author_name is not None:
+                    authors.append(author_name.strip())
+
+        for a in authors:
+            #e.g. "Von Max Maier und Sabine Braun" -> ["Max Maier", "Sabine Braun"]
+            a_copy = a.lower()
+            if a_copy.startswith('von'):
+                a_copy = a_copy[3:].strip()
+
+            author_names = [a.strip() for a in a_copy.split('und')]
+            authors.extend(author_names)
+            authors.remove(a)
+
+        yield {
+            'category': article.css('span.title::text').get().lower(),
+            'url': response.request.url,
+            'teaser': text[0].css("p strong::text").get().strip().lower(),
+            'date': header.css('span.article__date::text').get(),
+            'author':authors,
+            'is_update': header.css(
+                'span.article__kicker span:nth-child(0)::text').get(),  # does not work currently
+            'kicker': header.css('span.article__kicker::text').get().strip().lower(), 
+            'headline': header.css('span.article__headline::text').get().lower(),
+            'named_references': named_references,
+            'text_segments': article_text_blocks,
+            'tags': article_wrapper.css(
+                'section.article__tags ul li a::text').getall()
+        }
