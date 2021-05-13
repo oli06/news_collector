64bad5124453bfc569c3ce7c2824578844a5456d
Oliver Zernikow <zernikowoliver@gmail.com>
2021-05-13 12:13:01 +0200

    update tagesschau spider, new web design

--- news_collector/spiders/tagesschau_spider.py
+++ news_collector/spiders/tagesschau_spider.py
@@ -1,3 +1,4 @@
+
 import scrapy
 import logging
 import datetime
@@ -9,7 +10,7 @@ class TagesschauSpider(bs.BaseSpider):
     name = "tagesschau"
 
     def __init__(self):
-        super().__init__(self.name, 200, "https://www.tagesschau.de/", ['multimedia', '100sekunden', 'regional', 'thema'])
+        super().__init__(self.name, 500, "https://www.tagesschau.de/", ['multimedia', '100sekunden', 'regional', 'thema'])
 
     def start_requests(self):
         urls = [
@@ -20,15 +21,15 @@ class TagesschauSpider(bs.BaseSpider):
 
     def start_requests2(self):
         urls = [
-            'https://www.tagesschau.de/wirtschaft/coronavirus-fleischbetrieb-103.html'
+            'https://www.tagesschau.de/inland/bundeskabinett-klimaschutzgesetz-101.html'
         ]
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parseArticle)
 
     def parse(self, response):
-        for section in response.css('div.sectionA'): #first article is tagesthemen video
-            for a in section.css('div.con div.modCon div.mod div.boxCon div.box div.teaser div.mediaCon a.mediaLink'):
+        for section in response.css("div.container div.teasergroup"):
+            for a in section.css('a'):
                 yield response.follow(a, callback=self.parseArticle)
 
     def parseArticle(self, response):
@@ -46,16 +47,24 @@ class TagesschauSpider(bs.BaseSpider):
         self.total_parsed += 1
         logging.info(f"{self.total_parsed}. {url}")
 
-        article = response.css('div.storywrapper')
-        header = article.css('div.sectionA')
-        content = article.css('div.sectionZ')
+        article = response.css('article.container')
+        header = article.css('div.meldungskopf')
+        content = article
 
         #create item and add values
         article_item = NewsCollectorItem()
         article_item['raw'] = response.body.decode('utf-8')
-        article_item['headline'] = header.css('div.meldungHead span.headline::text').get().strip('\n').strip()
-        article_item['kicker'] = header.css('div.meldungHead span.dachzeile::text').get().strip('\n').strip()
-        article_item['date'] = header.css('div.meldungHead p.text span.stand::text').get().strip('\n').replace('Stand:', '').strip()
+        # article_item['headline'] = header.css('div.meldungskopf__title span.meldungskopf__headline--text::text').get().strip('\n').strip()
+
+        article_item["headline"] = ""
+        for span in header.xpath('//span[@class="meldungskopf__headline--text"]//text()'):
+            if(span):
+                article_item["headline"] += span.get()
+
+        article_item["headline"] = article_item["headline"].strip('\n').strip()
+
+        article_item['kicker'] = header.css('div.meldungskopf__title span.meldungskopf__topline::text').get().strip('\n').strip()
+        article_item['date'] = header.css('div.meldungskopf__title p.meldungskopf__datetime::text').get().strip('\n').replace('Stand:', '').strip()
         article_item['agency'] = self.name
         article_item['url'] = url
         article_item['category'] = category
@@ -63,41 +72,24 @@ class TagesschauSpider(bs.BaseSpider):
         article_item['named_references'] = {}
         article_item['text'] = ""
         article_item['teaser'] = ""
+        
         article_item['tags'] = []
+        for tag in content.css("div.meldungsfooter ul.taglist li.taglist__element a::text"):
+            article_item['tags'].append(tag.get())
+        
+        article_item["subheadlines"] = []
+        for subtitle in content.css("h2.meldung__subhead::text"):
+            article_item["subheadlines"].append(subtitle.get())
+        
+        #every tag holding text is marked with "textabsatz". Inside these tags we want the whole text (from all children)
+        plain_texts = content.xpath('//*[contains(@class, "textabsatz")]//text()')
+        for t in plain_texts: 
+            article_item['text'] += t.get().strip().strip('\n').strip() + " "
 
-        text_div = content.css('div.con div.modCon div.modParagraph')
-        for tag in text_div:
-            nodes = tag.xpath('./node()')
-            nodes_name = nodes.xpath('name()').get()
-            if nodes_name == 'div':
-                #divs contain other resources, advertisement, etc
-                #while text is in p-tags
-                continue
-
-            for node in nodes:
-                #some <p> are used for other things than text
-                class_attributes = node.xpath('@class').get()
-                if class_attributes is not None and 'autorenzeile' in class_attributes:
-                    #tagesschau doesnt use authors very well, therefore we dont rely on this data
-                    #e.g. https://www.tagesschau.de/ausland/belarus-militaerparade-105.html
-                    continue
-
-                child_nodes = node.xpath('./node()')                
-                for c in child_nodes:
-                    #print("before")
-                    node_name = c.xpath('name()').get()
-                    #print("after")
-                    if node_name == 'a':
-                        href = c.xpath('@href').get() #relative href
-                        article_item['named_references'][c.xpath('text()').get().strip('\n').replace('.', '%2E') if c.xpath('text()').get() is not None else 'unknown_' + href.replace('.', '%2E')] = href
-                        print(href)
-                        #yield response.follow(href, callback=self.parseArticle)
-                    elif node_name == None:
-                        article_item['text'] += c.get().strip('\n')
-                    elif node_name == 'h2': #subtitle
-                        article_item['text'] += c.get().strip('\n')
-                    elif node_name == 'strong':
-                        article_item['teaser'] += c.css('::text').get().strip('\n')
+        text_refs = content.xpath('//*[contains(@class, "textabsatz")]/a')
+        for ref in text_refs:
+            href = ref.xpath("@href").get()
+            article_item['named_references'][ref.xpath("text()").get().strip().strip('\n').strip().replace('.', '%2E') if ref.xpath("text()").get() is not None else 'unknown_' + href.replace('.', '%2E')] = href
 
         for x in article_item['named_references']:
             # print("now parsing: x: " + x + " and link is: " + article_item['named_references'][x])
